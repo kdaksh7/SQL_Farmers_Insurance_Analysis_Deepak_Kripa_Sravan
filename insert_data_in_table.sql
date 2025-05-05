b19e0060e09e7b298cc7c2fedeab7c0b20a70672
kdaksh7 <42858750+kdaksh7@users.noreply.github.com>
2025-05-05 18:41:23 +0530

    Update insert_data_in_table.sql

--- insert_data_in_table.sql
+++ insert_data_in_table.sql
@@ -10,56 +10,56 @@ CREATE SCHEMA IF NOT EXISTS ndap;
 
 USE ndap;
 
--- CREATE TABLE IF NOT EXISTS FarmersInsuranceData (
---     rowID INT PRIMARY KEY,
---     srcYear INT,
---     srcStateName VARCHAR(255),
---     srcDistrictName VARCHAR(255),farmersinsurancedata
---     InsuranceUnits INT,
---     TotalFarmersCovered INT,
---     ApplicationsLoaneeFarmers INT,
---     ApplicationsNonLoaneeFarmers INT,
---     InsuredLandArea FLOAT,
---     FarmersPremiumAmount FLOAT,
---     StatePremiumAmount FLOAT,
---     GOVPremiumAmount FLOAT,
---     GrossPremiumAmountToBePaid FLOAT,
---     SumInsured FLOAT,
---     PercentageMaleFarmersCovered FLOAT,
---     PercentageFemaleFarmersCovered FLOAT,
---     PercentageOthersCovered FLOAT,
---     PercentageSCFarmersCovered FLOAT,
---     PercentageSTFarmersCovered FLOAT,
---     PercentageOBCFarmersCovered FLOAT,
---     PercentageGeneralFarmersCovered FLOAT,
---     PercentageMarginalFarmers FLOAT,
---     PercentageSmallFarmers FLOAT,
---     PercentageOtherFarmers FLOAT,
---     YearCode INT,
---     Year_ VARCHAR(255),
---     Country VARCHAR(255),
---     StateCode INT,
---     DistrictCode INT,
---     TotalPopulation INT,
---     TotalPopulationUrban INT,
---     TotalPopulationRural INT,
---     TotalPopulationMale INT,
---     TotalPopulationMaleUrban INT,
---     TotalPopulationMaleRural INT,
---     TotalPopulationFemale INT,
---     TotalPopulationFemaleUrban INT,
---     TotalPopulationFemaleRural INT,
---     NumberOfHouseholds INT,
---     NumberOfHouseholdsUrban INT,
---     NumberOfHouseholdsRural INT,
---     LandAreaUrban FLOAT,
---     LandAreaRural FLOAT,
---     LandArea FLOAT
--- );
+CREATE TABLE IF NOT EXISTS FarmersInsuranceData (
+rowID INT PRIMARY KEY,
+srcYear INT,
+srcStateName VARCHAR(255),
+srcDistrictName VARCHAR(255),farmersinsurancedata
+InsuranceUnits INT,
+TotalFarmersCovered INT,
+ApplicationsLoaneeFarmers INT,
+ApplicationsNonLoaneeFarmers INT,
+InsuredLandArea FLOAT,
+FarmersPremiumAmount FLOAT,
+StatePremiumAmount FLOAT,
+GOVPremiumAmount FLOAT,
+GrossPremiumAmountToBePaid FLOAT,
+SumInsured FLOAT,
+PercentageMaleFarmersCovered FLOAT,
+PercentageFemaleFarmersCovered FLOAT,
+PercentageOthersCovered FLOAT,
+PercentageSCFarmersCovered FLOAT,
+PercentageSTFarmersCovered FLOAT,
+PercentageOBCFarmersCovered FLOAT,
+PercentageGeneralFarmersCovered FLOAT,
+PercentageMarginalFarmers FLOAT,
+PercentageSmallFarmers FLOAT,
+PercentageOtherFarmers FLOAT,
+YearCode INT,
+Year_ VARCHAR(255),
+Country VARCHAR(255),
+StateCode INT,
+DistrictCode INT,
+TotalPopulation INT,
+TotalPopulationUrban INT,
+TotalPopulationRural INT,
+TotalPopulationMale INT,
+TotalPopulationMaleUrban INT,
+TotalPopulationMaleRural INT,
+TotalPopulationFemale INT,
+TotalPopulationFemaleUrban INT,
+TotalPopulationFemaleRural INT,
+NumberOfHouseholds INT,
+NumberOfHouseholdsUrban INT,
+NumberOfHouseholdsRural INT,
+LandAreaUrban FLOAT,
+LandAreaRural FLOAT,
+LandArea FLOAT
+);
 
--- LOAD DATA LOCAL INFILE "path\data.csv"
--- INTO TABLE FarmersInsuranceData
--- FIELDS TERMINATED BY ','  
--- ENCLOSED BY '"'          
--- LINES TERMINATED BY '\n' 
--- IGNORE 1 ROWS;
+LOAD DATA LOCAL INFILE "path\data.csv"
+INTO TABLE FarmersInsuranceData
+FIELDS TERMINATED BY ','  
+ENCLOSED BY '"'          
+LINES TERMINATED BY '\n' 
+IGNORE 1 ROWS;
